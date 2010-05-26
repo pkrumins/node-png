326b9ab377feaf86fbbb2b0c87729185329c0904
Peteris Krumins <peteris.krumins@gmail.com>
2010-05-26 22:08:29 +0300

    added a test for DynamicPngStack

---
+++ tests/dynamic-png-stack.js
@@ -0,0 +1,27 @@
+var PngLib = require('png');
+var fs = require('fs');
+var sys = require('sys');
+var Buffer = require('buffer').Buffer;
+
+var pngStack = new PngLib.DynamicPngStack();
+
+function rectDim(fileName) {
+    var m = fileName.match(/^\d+-rgba-(\d+)-(\d+)-(\d+)-(\d+).dat$/);
+    var dim = [m[1], m[2], m[3], m[4]].map(function (n) {
+        return parseInt(n, 10);
+    });
+    return { x: dim[0], y: dim[1], w: dim[2], h: dim[3] }
+}
+
+var files = fs.readdirSync('./push-data');
+
+files.forEach(function(file) {
+    var dim = rectDim(file);
+    var rgba = fs.readFileSync('./push-data/' + file, 'binary');
+    var buf = new Buffer(rgba.length);
+    buf.write(rgba, 'binary');
+    pngStack.push(buf, dim.x, dim.y, dim.w, dim.h);
+});
+
+fs.writeFileSync('dynamic.png', pngStack.encode(), 'binary');
+
